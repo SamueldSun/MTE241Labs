00d1449696586dd9d09e5b9bcde05f19500bc899
clayhaight01 <clayhaight@yahoo.ca>
2021-10-05 15:18:45 -0400

    problem 3

--- lab2/src/main.c
+++ lab2/src/main.c
@@ -14,38 +14,82 @@ Joystick E: P1.24
 */
 
 //section selection
-//#define section 3
-/*
-void set_LED (int LED_num, int val) {
+#define section 3
+
+void set_LED (int LED_num, int val) { //val = 0 is off, val = 1 is on, val = 2 is all LEDs off
+	int port = 0;
+	switch(LED_num) {
+		case 1:
+			LED_num = 28;
+			port = 1;
+			break;
+		case 2:
+			LED_num = 29;
+			port = 1;
+			break;
+		case 3:
+			LED_num = 31;
+			port = 1;
+			break;
+		case 4:
+			LED_num = 2;
+			port = 2;
+			break;
+		case 5:
+			LED_num = 3;
+			port = 2;
+			break;
+		case 6:
+			LED_num = 4;
+			port = 2;
+			break;
+		case 7:
+			LED_num = 5;
+			port = 2;
+			break;
+		case 8:
+			LED_num = 6;
+			port = 2;
+			break; 
+	}
 	LPC_GPIO1->FIODIR |= ((1u << 28) | (1u << 29) | (1u << 31));
 	LPC_GPIO2->FIODIR |= ((1u << 2) | (1u << 3) | (1u << 4) | (1u << 5) | (1u << 6));
-	LPC_GPIO1->FIOCLR |= ((1u << 28) | (1u << 29) | (1u << 31));
-	LPC_GPIO2->FIOCLR |= ((1u << 2) | (1u << 3) | (1u << 4) | (1u << 5) | (1u << 6));
-	
-	if (val==1) { //LED on
+
+	if(val==2 )	{//all LEDs off
+		LPC_GPIO1->FIOCLR |= ((1u << 28) | (1u << 29) | (1u << 31));
+		LPC_GPIO2->FIOCLR |= ((1u << 2) | (1u << 3) | (1u << 4) | (1u << 5) | (1u << 6));
+		return;
+	}
+	else if (val==1 && port==1) {	//LED on
 		LPC_GPIO1->FIOCLR |= (1u << LED_num);
 		LPC_GPIO1->FIOSET |= (1u << LED_num);
 	}
-	else { //LED off
-		LPC_GPIO1->FIOCLR = (1u << LED_num);
+	else if(val==1 && port==2) {	//LED on
+		LPC_GPIO2->FIOCLR |= (1u << LED_num);
+		LPC_GPIO2->FIOSET |= (1u << LED_num);
 	}
+	else if(val==0 && port==1)	//LED off
+		LPC_GPIO1->FIOCLR = (1u << LED_num);
+	else	//LED off
+		LPC_GPIO2->FIOCLR = (1u << LED_num);
 }
 
 #if section == 1
 int main(void) //1. push button LED
 {
 SystemInit();
+set_LED(0,2); //set all LEDs off
 while(1){
 if((LPC_GPIO2->FIOPIN & (1u << 10))) {//set LED off
-	set_LED(28,0);
+	set_LED(1,0);
 }
 else { //set LED on
-	set_LED(28,1);
+	set_LED(1,1);
 }
 }
 }
 #elif section == 2
-int main(void) //1. push button LED
+int main(void) //2. joystick
 {
 SystemInit();
 while(1){
@@ -75,21 +119,26 @@ else {
 }
 }
 #elif section == 3
-*/
-int main(void) //1. push button LED
+int main(void) //3. uart LED
 {
-int receive[100] = {0};
 SystemInit();
 UARTInit(1, 115200);
-
+set_LED(0,2); //set all LEDs off
 while(1){
-	for(int x=0; x<100; x++) {
-		receive[x] = UARTReceiveChar(1);
-		printf("beach");
+	int curr_input = 0;
+	int input_sum = 0;
+	while(curr_input != 10) {
+		curr_input = UARTReceiveChar(1);
+		if(curr_input != 10)
+			input_sum = 10*input_sum+(curr_input-48);
 	}
-//ascii 10
-//-30
-//convert to binary
+	set_LED(0,2);
+	for(int i=0; i<8; i++) {
+		int bitmask = (1u << i);
+		if((input_sum & (1u << i))) {
+			set_LED(i+1,1);
+		}
+	}
 }
 }
-//#endif
+#endif
